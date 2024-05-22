fe98355410e4be8cdd06e7944d034a6ff884530a
Hudson Ruger <rugerhudson@icloud.com>
2024-05-22 19:31:33 -0400

    Initial Commit

---
+++ SessionTracker 2.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SessionTracker 2.0
+//
+//  Created by Hudson Ruger on 5/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SessionTracker 2.0/SessionTracker_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  SessionTracker_2_0App.swift
+//  SessionTracker 2.0
+//
+//  Created by Hudson Ruger on 5/22/24.
+//
+
+import SwiftUI
+
+@main
+struct SessionTracker_2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
